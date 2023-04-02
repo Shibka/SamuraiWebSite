604feb4958a4030d762617ad05a6311616ddab63
Aliaksei <shebalinas2000@gmail.com>
2023-04-02 13:09:16 +0200

    Export

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,19 @@
 import React from "react";
 import "./App.css";
-import Accordion from "./components/Accordion/Accordion";
-import {Rating} from "./components/Rating/Rating";
+import Header from "./Header";
+import Technologies from "./Technologies";
+import Footer from "./Footer";
 
-function App() {
-    console.log("App rendering");
 
+const App = () => {
     return (
         <div>
-            <PageTitle title={"This is App component"}/>
-            <PageTitle title={"My friends"}/>
-            Article 1
-            <Rating value={0}/>
-            <Accordion titleValue={"Menu"} collapsed={true}/>
-            <Accordion titleValue={"Users"} collapsed={false}/>
-            Article 2
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+            <Header/>
+            <Technologies/>
+            <Footer />
         </div>
     );
 }
 
-type PageTitlePropsType = {
-    title: string;
-};
-
-function PageTitle(props: PageTitlePropsType) {
-    console.log("PageTitle rendering");
-    return <h1>{props.title}</h1>;
-}
 
 export default App;
---
+++ src/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+    return (<div>
+        <a href="#s">Home</a> -
+        <a href="#s">News Feed</a> -
+        <a href="#s">Message</a> -
+    </div>);
+}
+
+
+export default Footer;
