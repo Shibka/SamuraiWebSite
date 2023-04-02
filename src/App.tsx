06fad01fe1e0d36482f6eb9f9de234f6bbd857db
Aliaksei <shebalinas2000@gmail.com>
2023-04-02 13:55:46 +0200

    JS

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Footer from "./Footer";
 
 const App = () => {
     return (
-        <div>
+        <div> fdfdf
             <header>
                 <img src="" alt="Logo"/>
             </header>
