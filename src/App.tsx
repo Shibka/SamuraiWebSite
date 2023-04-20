fcc871e5c424aa3f3398a2aa2807dca12751b0f4
Aliaksei <shebalinas2000@gmail.com>
2023-04-20 21:53:37 +0200

    New

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import logo from './Images/Logo/logo.png';
+import MainBackground from './Images/MainBackground/MainBackground.jpg'
 
 const App = () => {
     return (
@@ -15,7 +16,29 @@ const App = () => {
                 <div><a>Music</a></div>
                 <div><a>Settings</a></div>
             </nav>
-            <div className='content'>Main Content</div>
+            <div className='content'>
+                <div>
+                    <img src={MainBackground} alt="Background" width={1000}/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 }
