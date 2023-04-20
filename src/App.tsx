e354a34872138253d2e5880f5108420d9ddf90bc
Aliaksei <shebalinas2000@gmail.com>
2023-04-20 21:39:01 +0200

    New

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import "./App.css";
-import logo from './Images/logo.png';
+import logo from './Images/Logo/logo.png';
 
 const App = () => {
     return (
-        <div>
-            <header>
+        <div className='app-wrapper'>
+            <header className='header'>
                 <img src={logo} alt="Logo" width={150}/>
             </header>
-            <nav>
-                <div>Profile</div>
-                <div>Messages</div>
+            <nav className='nav'>
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
             </nav>
-            <div>Main Content</div>
+            <div className='content'>Main Content</div>
         </div>
     );
 }
