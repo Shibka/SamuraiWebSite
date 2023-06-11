f33e7d9b8d260428ff97ed5b57be230a36aa1e05
Aliaksei <shebalinas2000@gmail.com>
2023-06-11 16:42:06 +0200

    New

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,26 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
     return (
+        <BrowserRouter>
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
             <div className='app-wrapper-content'>
-                <Dialogs/>
+                <Route path={'/dialogs'} component={Dialogs}/>
+                <Route path={'/profile'} component={Profile}/>
+                <Route path={'/news'} component={News}/>
+                <Route path={'/music'} component={Music}/>
+                <Route path={'/settings'} component={Settings}/>
             </div>
-            {/*<Profile/>*/}
         </div>
+        </BrowserRouter>
     );
 }
 
