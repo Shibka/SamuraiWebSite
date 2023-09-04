a85742dee062b9a11ca62e8a580d229a1eb52777
Aliaksei <shebalinas2000@gmail.com>
2023-09-04 13:02:36 +0200

    New

--- src/index.tsx
+++ src/index.tsx
@@ -2,26 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import {state} from "./redux/state";
 
-
-let posts = [
-    {id: 1, message: 'Hello, how you doin?', likesCount: 12},
-    {id: 2, message: 'Hi, great, thanx', likesCount: 10},
-]
-let dialogs = [
-    {id: 1, name: 'Alex'},
-    {id: 2, name: 'Igor'},
-    {id: 3, name: 'Leha'},
-    {id: 4, name: 'Gleb'}
-]
-let messages = [
-    {id: 1, message: 'Hi'},
-    {id: 2, message: 'Hi Hi how are you?'},
-    {id: 3, message: 'Hi Hi Hi'}
-]
-
-ReactDOM.render(<App
-    posts={posts}
-    messages={messages}
-    dialogs={dialogs}
+ReactDOM.render(
+    <App
+    posts={state.profile.posts}
+    messages={state.messagesPage.messages}
+    dialogs={state.messagesPage.dialogs}
 />, document.getElementById("root"));
---
+++ src/redux/state.js
@@ -0,0 +1,22 @@
+export let state = {
+    profile: {
+        posts: [
+            {id: 1, message: 'Hello, how you doin?', likesCount: 12},
+            {id: 2, message: 'Hi, great, thanx', likesCount: 10},
+        ],
+    },
+    messagesPage: {
+        dialogs: [
+            {id: 1, name: 'Alex'},
+            {id: 2, name: 'Igor'},
+            {id: 3, name: 'Leha'},
+            {id: 4, name: 'Gleb'}
+        ],
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'Hi Hi how are you?'},
+            {id: 3, message: 'Hi Hi Hi'}
+        ]
+    },
+
+}
