ca2c647d56b91dd3310b03bf1a796cacfff151db
Aliaksei <shebalinas2000@gmail.com>
2023-09-18 19:06:45 +0200

    New

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,61 @@
-import {rerenderEntireTree} from "./render";
-import {addPost, state, updatePostMessage} from "./redux/state";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import {addPost, state, StateType, subscribe, updatePostMessage} from "./redux/state";
 
+// type ProfileStructureType = {
+//     id: number;
+//     message: string;
+//     likesCount: number;
+// };
+// type DialogsStructureType = {
+//     id: number;
+//     name: string;
+// };
+// type MessagesStructureType = {
+//     id: number;
+//     message: string;
+// };
+// type FriendsStructureType = {
+//     id: number;
+//     name: string;
+//     message: string;
+// };
+// type renderPropsType = {
+//     state: {
+//         profile: {
+//             posts: ProfileStructureType[];
+//             newPostText: string
+//         };
+//         messagesPage: {
+//             dialogs: DialogsStructureType[];
+//             messages: MessagesStructureType[];
+//         };
+//         sidebar: {
+//             friends: FriendsStructureType[];
+//         };
+//     };
+//     addPost: (postMessage: string) => void;
+//     updatePostMessage: (newPostText: string) => void
+//     subscribe: (observer: () => void) => void
+// };
 
-rerenderEntireTree({state, addPost, updatePostMessage})
+export const rerenderEntireTree = (props: StateType) => { //пропсы можно и не передавать и сделать все с пустой функцией !!!!
+
+    ReactDOM.render(
+        <React.StrictMode>
+            <App
+                addPost={addPost}
+                posts={state.profile.posts}
+                messages={state.messagesPage.messages}
+                dialogs={state.messagesPage.dialogs}
+                newPostMessage={state.profile.newPostText}
+                updatePostMessage={updatePostMessage}
+            />
+        </React.StrictMode>,
+        document.getElementById("root")
+    );
+};
+rerenderEntireTree(state) // тут может быть пусто как и все функции в state
+subscribe(rerenderEntireTree)
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,4 +1,5 @@
-import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = (state:StateType) => {
+}
 
 type ProfileStructureType = {
     id: number;
@@ -18,60 +19,63 @@ type FriendsStructureType = {
     name: string;
     message: string;
 };
-type StateType = {
+export type StateType = {
 
-        profile: {
-            posts: ProfileStructureType[];
-            newPostText: string
-        };
-        messagesPage: {
-            dialogs: DialogsStructureType[];
-            messages: MessagesStructureType[];
-        };
-        sidebar: {
-            friends: FriendsStructureType[];
-        };
-    }
+    profile: {
+        posts: ProfileStructureType[];
+        newPostText: string
+    };
+    messagesPage: {
+        dialogs: DialogsStructureType[];
+        messages: MessagesStructureType[];
+    };
+    sidebar: {
+        friends: FriendsStructureType[];
+    };
+}
 export let state: StateType = {
-        profile: {
-            posts: [
-                {id: 1, message: 'Hello, how you doin?', likesCount: 12},
-                {id: 2, message: 'Hi, great, thanx', likesCount: 10},
-            ],
-            newPostText: '',
-        },
-        messagesPage: {
-            dialogs: [
-                {id: 1, name: 'Alex'},
-                {id: 2, name: 'Igor'},
-                {id: 3, name: 'Leha'},
-                {id: 4, name: 'Gleb'}
-            ],
-            messages: [
-                {id: 1, message: 'Hi'},
-                {id: 2, message: 'Hi Hi how are you?'},
-                {id: 3, message: 'Hi Hi Hi'}
-            ]
-        },
-        sidebar: {
-            friends: [
-                {id: 2, name: 'Igor',message: 'Hi'},
-                {id: 3, name: 'Leha',message: 'Hi'},
-                {id: 4, name: 'Gleb',message: 'Hi'}
-            ]
-        },
+    profile: {
+        posts: [
+            {id: 1, message: 'Hello, how you doin?', likesCount: 12},
+            {id: 2, message: 'Hi, great, thanx', likesCount: 10},
+        ],
+        newPostText: '',
+    },
+    messagesPage: {
+        dialogs: [
+            {id: 1, name: 'Alex'},
+            {id: 2, name: 'Igor'},
+            {id: 3, name: 'Leha'},
+            {id: 4, name: 'Gleb'}
+        ],
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'Hi Hi how are you?'},
+            {id: 3, message: 'Hi Hi Hi'}
+        ]
+    },
+    sidebar: {
+        friends: [
+            {id: 2, name: 'Igor', message: 'Hi'},
+            {id: 3, name: 'Leha', message: 'Hi'},
+            {id: 4, name: 'Gleb', message: 'Hi'}
+        ]
+    },
 
-    }
+}
 
 
- export const addPost = (postMessage: string) => {
-     const newPost = { id: 5, message: postMessage, likesCount: 3 };
-     state.profile.posts.push(newPost);
-     updatePostMessage('')
-     rerenderEntireTree({state,addPost, updatePostMessage})
- };
+export const addPost = (postMessage: string) => {
+    const newPost = {id: Math.random(), message: postMessage, likesCount: 3};
+    state.profile.posts.push(newPost);
+    updatePostMessage('')
+    rerenderEntireTree(state)
+};
 export const updatePostMessage = (newPostText: string) => {
-
     state.profile.newPostText = newPostText
-     rerenderEntireTree({state,addPost, updatePostMessage})
- };
+    rerenderEntireTree(state)
+};
+
+export const subscribe = (observer: (state:StateType) => void) => {
+    rerenderEntireTree = observer
+}
